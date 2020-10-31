7b7d035cd5f11371e2e5ffc966ec0881f9eed3a5
aariv <aariv@gmail.com>
2020-10-31 12:34:49 +0530

    level1 html and css

---
+++ level1/variablesandconstants/script.js
@@ -0,0 +1,72 @@
+// single line comment
+
+/*
+first line
+second line
+*/
+
+// print
+// console.log('Hello world');
+
+// variables
+// int and float in c
+// but in JS, it is just number
+/*
+var $age = 27.5;
+console.log("my age is ", $age);
+console.log(typeof($age));
+
+var name = "Shrikanth";
+var age = 23;
+var college = "Boston"
+
+console.log(" Hi my name is " + name + " my age is " + age + ". and I am from " + college);
+
+console.log("hi my name is",name,"my name is",age);
+
+var sum = 83+4*5-72/2**3;
+console.log(sum);
+
+var sum1 = 627-3*(14-4**2+17)**2/(25-16);
+console.log(sum1);
+*/
+// datatypes  in details
+// datatypes or primitives
+/*
+datatypes in js :
+1. Number : int and float
+2. String 
+3. Undefined
+4. Null
+5. Boolean
+*/
+
+// var a = 2;
+// console.log(typeof a);
+
+// var b = 23.5;
+// console.log(typeof b);
+
+// var c = '2';
+// console.log(typeof c);
+
+// var d;
+// // you are not caring about the variable
+// console.log(typeof d);
+
+// // null : you are saying it has nothing
+// var e = null;
+// console.log(typeof e);
+// var f = true;
+// console.log(typeof f);
+
+// var g = [1, 2, 3, 'a'];
+// console.log(g);
+// console.log(typeof g);
+
+// var n = 23;
+// console.log(window.n);
+// console.log(window);
+
+
+
---
+++ level1/variablesandconstants/scripts/variablesandconstants.js
@@ -0,0 +1,14 @@
+// let a = "hi";
+// a =3
+// console.log(a);
+
+
+// // constant means you cant change the value
+
+// const b = 3;
+// // b = "abs" // assignment to a constant variable
+// console.log(b);
+
+// // Note : use constants as often as possible to be clear from the beginning of your project
+
+let curren
